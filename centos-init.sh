bf88fdbe4fbaf151f69b9b3f8736b38a09427ac8
zmysky <z.mysky@qq.com>
2019-03-19 17:45:25 +0800

    调整脚本

--- centos-init.sh
+++ centos-init.sh
@@ -6,20 +6,14 @@
 # https://opsx.alibaba.com/mirror
 # http://mirrors.aliyun.com/repo/
 
-# 安装 curl
-yum install -y curl
+# 安装 curl 与 EPEL 源
+yum install -y curl epel-release
 
 # 备份 CentOS-Base.repo
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 
 # 使用阿里 YUM 源
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-
-# 清除 YUM 缓存 && 更新 YUM 缓存
-yum clean all && yum makecache
-
-# 安装 EPEL 源
-yum install -y epel-release
 
 # 使用阿里 EPEL 源
 wget -O /etc/yum.repos.d/epel-7.repo http://mirrors.aliyun.com/repo/epel-7.repo
@@ -33,7 +27,7 @@ groupadd -g 33 www-data
 
 # -s：指定用户所用的 shell，/sbin/nologin，表示不登录。
 # -M：不创建用户主目录
-useradd -u 33 -g www-data -s /sbin/nologin -M
+useradd -u 33 -g www-data -s /sbin/nologin -M www-data
 
 chown -R www-data:www-data /websvr/
 
@@ -54,11 +48,11 @@ sudo usermod -aG docker www-data
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# 配置镜像加速器(阿里)
+# 配置镜像加速器(七牛)
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://fm9u2lxx.mirror.aliyuncs.com"]
+  "registry-mirrors": ["https://reg-mirror.qiniu.com"]
 }
 EOF
 sudo systemctl daemon-reload
